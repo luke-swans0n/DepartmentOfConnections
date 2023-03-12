ce2506acddb50331fb46944d14e2dc8d3761fef2
mrlukeswanson <mr.luke.swanson@gmail.com>
2023-03-12 18:32:16 -0500

    initial commit

--- app.py
+++ app.py
@@ -15,11 +15,6 @@ import json
 from flask import Flask
 import os
 
-config = json.load(open("config.json", 'r'))
-
-account_sid = config['account']
-
-auth_token = config['auth_token']
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -28,12 +23,13 @@ app.secret_key = os.urandom(24)
 @app.route('/', methods=['POST'])
 def home():
     response = VoiceResponse()
-    response.play("", digits='wwwwwwwwwwwwwwwwwwww0')
+    response.play("", digits='wwwwwwwwwwwwwwwwwwwwwwwwww0w0')
+    response.play("", digits='wwwwwwwwwwwwwwwwwwwwwwwwwwwwww0')
     with response.gather(
             numDigits=10, action=url_for('get_name'), method="POST"
     ) as g:
         g.say("Please enter a 10-digit phone number to send a message to.",
-              voice="alice", language="en-GB")
+              voice="alice", language="en-GB", loop=4)
     return str(response)
 
 
@@ -57,19 +53,21 @@ def get_name():
     response = VoiceResponse()
     with response.gather(input='speech', action='send_sms', profanity_filter=False) as g:
         g.say(
-            f"Please state your name.",
+            "Please state your name.",
             voice='alice', language='en-GB')
     return str(response)
 
 
 @app.route('/sent_sms', methods=['POST'])
 def sent_sms():
+    account='AC99f284918b8ee54f80f5b6f67fbd1bed'
+    auth_token = '1a2f3512ce6f0f69d8cc75a0a3fd5a77'
     phone_number = session['phone_number']
     username = session['username']
     message = request.form['SpeechResult']
-    preamble = f"Hello this is The Department of Connections, a messaging service for people stuck in the Cook County Department of Corrections. We have received the following message for you from {username}: "
-    postmessage = """To keep this service running, we ask that you donate $1 via CashApp to @polynomial (Luke Swanson). We are not officially affiliated with Cook County, the Department of Corrections, or any other governmental entity."""
-    client = Client(account_sid, auth_token)
+    preamble = f'Hello this is The Department of Connections, a messaging service for people stuck in the Cook County Department of Corrections. We have received the following message for you from {username}: '
+    postmessage = """To keep this service running, we ask that you donate $1 via CashApp to $polynomial (Luke Swanson). We are not officially affiliated with Cook County, the Department of Corrections, or any other governmental entity."""
+    client = Client(account, auth_token)
     client.messages.create(
         messaging_service_sid='MGde22decc327802a222043e6fd3b6a1ca',
         body=preamble,
@@ -89,4 +87,3 @@ def sent_sms():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
-    app.run(debug=True)
---
+++ deptdb.py
@@ -0,0 +1,6 @@
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
